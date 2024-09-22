b2d67b043364d79734620711fceab3fd119be18c
Dipendra singh <dipendravishnu@gmail.com>
2024-09-22 22:34:03 +0530

    commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public'))); // Use app.use here
+
+io.on('connection',function (socket)  {
+    socket.on("send-location", function (data) {
+    io.emit("receive-location", {id: socket.id, ...data});
+    });
+    socket.on("disconnect", function() {
+        io.emit("user-disconnected", socket.id); 
+    });
+});
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+git 
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
